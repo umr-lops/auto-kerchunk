9d6809c5aecbf371f3b6bd141d41a6c20bceb7a2
MAGIN <justus.magin@ifremer.fr>
2021-11-25 13:58:16 +0100

    add a `__name__` fence to `__main__.py`
    apparently, the most recent version of `pytest` tries to import
    `__main__.py` in doctest mode (this has already been fixed but not
    released, yet)

--- auto_kerchunk/__main__.py
+++ auto_kerchunk/__main__.py
@@ -1,4 +1,8 @@
 from .main import app
 
 
-app()
+# not adding this guard causes `pytest --doctest-modules .` to fail:
+# it is trying to import this file, which causes `typer`/`click` to
+# raise an error because they obviously don't know `pytest` options
+if __name__ == "__main__":
+    app()
