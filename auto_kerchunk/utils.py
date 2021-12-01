a7f6aba33e1bd6ecdb718db86725a82c89c27c1b
MAGIN <justus.magin@ifremer.fr>
2021-12-01 13:49:22 +0100

    default the scheme to "file"

--- auto_kerchunk/utils.py
+++ auto_kerchunk/utils.py
@@ -1,7 +1,7 @@
 def parse_url(url):
     pattern = "://"
     if pattern not in url:
-        return "", url
+        return "file", url
 
     scheme, path = url.split(pattern)
     return scheme, path
