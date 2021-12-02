dced1454c251d13875020b400f9a0fbb7cceb886
MAGIN <justus.magin@ifremer.fr>
2021-12-02 17:35:41 +0100

    use a lambda instead of attrgetter

--- auto_kerchunk/compression.py
+++ auto_kerchunk/compression.py
@@ -1,8 +1,7 @@
 import enum
-import operator
 
 import fsspec.compression
 
 names = [str(n).lower() for n in fsspec.compression.compr.keys()]
 CompressionAlgorithms = enum.Enum("CompressionAlgorithms", {n: n for n in names})
-CompressionAlgorithms.__str__ = operator.attrgetter("name")
+CompressionAlgorithms.__str__ = lambda self: self.name
