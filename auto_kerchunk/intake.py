31addc94c05a7aff040682a218623c8ddbb8132e
MAGIN <justus.magin@ifremer.fr>
2021-12-08 13:43:07 +0100

    change how the storage options are constructed

--- auto_kerchunk/intake.py
+++ auto_kerchunk/intake.py
@@ -2,7 +2,7 @@ from intake.catalog import Catalog  # noqa: F401
 from intake.catalog.local import LocalCatalogEntry
 
 
-def create_catalog_entry(name, description, url, storage_options):
+def create_catalog_entry(name, description, url, storage_options={}):
     """create a catalog entry
 
     Parameters
@@ -14,16 +14,14 @@ def create_catalog_entry(name, description, url, storage_options):
     url : str
         url to the kerchunk metadata file
     """
+    storage_options = {"fo": url} | storage_options
     entry = LocalCatalogEntry(
         name=name,
         description=description,
         driver="zarr",
         args={
             "urlpath": "reference://",
-            "storage_options": {
-                "fo": url,
-                **storage_options,
-            },
+            "storage_options": storage_options,
         },
     )
     return entry
