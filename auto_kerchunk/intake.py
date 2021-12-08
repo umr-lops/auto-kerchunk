2bdf130006ded1059f24cca765352025ac5f3072
MAGIN <justus.magin@ifremer.fr>
2021-12-08 11:43:55 +0100

    rough implementation of the intake creation

---
+++ auto_kerchunk/intake.py
@@ -0,0 +1,29 @@
+from intake.catalog import Catalog  # noqa: F401
+from intake.catalog.local import LocalCatalogEntry
+
+
+def create_catalog_entry(name, description, url, storage_options):
+    """create a catalog entry
+
+    Parameters
+    ----------
+    name : str
+        name of the catalog entry
+    description : str
+        additional description of the catalog
+    url : str
+        url to the kerchunk metadata file
+    """
+    entry = LocalCatalogEntry(
+        name=name,
+        description=description,
+        driver="zarr",
+        args={
+            "urlpath": "reference://",
+            "storage_options": {
+                "fo": url,
+                **storage_options,
+            },
+        },
+    )
+    return entry
